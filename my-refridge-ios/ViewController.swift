749f4ef4ac1ddcd3f3f22640560cd4db4b05f5bb
ddosang <218926@naver.com>
2021-02-11 19:45:23 +0900

    Initial Commit

---
+++ my-refridge-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my-refridge-ios
+//
+//  Created by 임은지 on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
